ae167d4a1e7cdefcfe96dfe578ac86bc1bee95ba
kbethany <apemi@mail.ru>
2019-09-13 13:28:33 +0300

    start

---
+++ lem_in.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem_in.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbethany <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/07 19:28:48 by kbethany          #+#    #+#             */
+/*   Updated: 2019/09/07 19:51:15 by kbethany         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include "../ft_printf/include/head.h"
+​
+typedef struct		s_data
+{
+	char						*name;
+	short						links_number;
+	short						to_start;
+	short						to_finish;
+}									t_data;
+​
+typedef struct		s_node
+{
+	t_data					data;
+	struct	s_node	**links;
+}									t_node;
+​
+t_data	mk_data(char *name);
+t_node	*mk_node(char *name, short links_number);
+
+#endif
